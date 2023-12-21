55900d1ae866faecc5ab76a23defec2319495c9e
Anjali Karimpil <anjaliskarimpil@gmail.com>
2023-12-21 21:48:13 +0000

    Implemented all functionality for basketball scorecard

---
+++ index.js
@@ -0,0 +1,25 @@
+function plusOne_home(){
+    let homePointsEl = document.getElementById("home-points-el")
+    homePointsEl.textContent = parseInt(homePointsEl.innerText) + 1
+}
+function plusTwo_home(){
+    let homePointsEl = document.getElementById("home-points-el")
+    homePointsEl.textContent = parseInt(homePointsEl.innerText) + 2
+}
+function plusThree_home(){
+    let homePointsEl = document.getElementById("home-points-el")
+    homePointsEl.textContent = parseInt(homePointsEl.innerText) + 3
+}
+
+function plusOne_guest(){
+    let guestPointsEl = document.getElementById("guest-points-el")
+    guestPointsEl.textContent = parseInt(guestPointsEl.innerText) + 1
+}
+function plusTwo_guest(){
+    let guestPointsEl = document.getElementById("guest-points-el")
+    guestPointsEl.textContent = parseInt(guestPointsEl.innerText) + 2
+}
+function plusThree_guest(){
+    let guestPointsEl = document.getElementById("guest-points-el")
+    guestPointsEl.textContent = parseInt(guestPointsEl.innerText) + 3
+}
